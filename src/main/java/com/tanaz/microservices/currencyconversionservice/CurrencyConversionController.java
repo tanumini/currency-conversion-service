98b9d9cd8be62710407effdb75ca0aca4b04d80b
unknown <tanazshaik1709@gmail.com>
2024-03-08 18:05:20 +0530

    added load balancing and naming server and tracing

--- src/main/java/com/tanaz/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/tanaz/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -5,24 +5,40 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@Configuration(proxyBeanMethods = false)
+class RestTemplateConfiguration {
+    
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+}
 @RestController
 public class CurrencyConversionController {
  
 	@Autowired
 	private CurrencyExhangeServiceProxy proxy;
+	
+	
+    @Autowired
+    private RestTemplate restTemplate;
+    
 	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrency(
 			@PathVariable String from, @PathVariable String to,@PathVariable BigDecimal quantity) {
 		Map<String,String> uriVariables= new HashMap<String,String>();
 		uriVariables.put("from",from);
 		uriVariables.put("to", to);
-		ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+		ResponseEntity<CurrencyConversionBean> responseEntity = restTemplate.getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
 				CurrencyConversionBean.class,uriVariables);
 		CurrencyConversionBean response = responseEntity.getBody();
 		return new CurrencyConversionBean(response.getId(),from,to,response.getConversionMultiple(),quantity,quantity.multiply(response.getConversionMultiple()),response.getPort());
--- src/main/java/com/tanaz/microservices/currencyconversionservice/CurrencyExhangeServiceProxy.java
+++ src/main/java/com/tanaz/microservices/currencyconversionservice/CurrencyExhangeServiceProxy.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-conversion-service",url="localhost:8000")
+//@FeignClient(name="currency-conversion-service",url="localhost:8000")
+@FeignClient(name="currency-exchange-service")
 public interface CurrencyExhangeServiceProxy {
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversionBean retrieveExchangeValue(
